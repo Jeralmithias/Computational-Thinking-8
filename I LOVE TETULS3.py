86b952220882d85a6beba364976e32beba9c90d2
Jeralmithias <sebsamano@seattleacademy.org>
2025-04-03 21:22:34 +0000

    somemassage

---
+++ I LOVE TETULS3.py
@@ -0,0 +1,19 @@
+import turtle
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-100, -100)
+t.color("red")
+t.pendown()
+
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+
+turtle.exitonclick()
